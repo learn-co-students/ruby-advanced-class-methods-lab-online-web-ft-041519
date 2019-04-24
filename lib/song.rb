cf98c19d72dcf1360632ba2db8549a0ba0595b26
Erin Stalzer <erinstalzer@gmail.com>
2019-04-24 13:39:35 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,35 @@ class Song
     self.class.all << self
   end
 
+  def create
+    self.new.save
+  end
+
+  def new_by_name(name)
+    @name = name
+  end
+
+  def create_by_name(name)
+    def initialize(name)
+      @name = name
+    end
+    @name.save
+  end
+
+  def find_by_name
+    if @@all.include?(self.name)
+    else
+      falsey
+    end
+  end
+
+  def find_or_create_by_name
+    if self.find_by_name
+      self
+    else self.create_by_name
+    end
+  end
+
+
+
 end
