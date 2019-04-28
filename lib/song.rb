22a6b6e296728bb4001433615d2907915f9730aa
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-28 20:49:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,3 +11,33 @@ class Song
   end
 
 end
+
+class Song.create
+
+def initalize
+
+song = Song.create 
+Song.all.include?(song) 
+end 
+
+class Song.new_by_name 
+  def Song.new_by_name
+  song = Song.new_by_name("The Middle")
+    @name="The Middle"
+    song.name => "The Middle"
+  end
+  
+  def Song.new_by_name
+    song = Song.new_by_name("The Middle")
+=> <Song @name="The Middle">
+song.name => "The Middle"
+end 
+
+def Song.create_by_name
+song = Song.create_by_name("The Middle")
+ @name="The Middle"
+song
+
+Song.all.include?(song)
+=> true
+    
